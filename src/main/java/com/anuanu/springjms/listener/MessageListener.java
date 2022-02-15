535216028958f42aec60cea8565f088269b76a84
Anupam <anusrivastava910@gmail.com>
2022-02-15 21:36:54 +0530

    Add files

--- src/main/java/com/anuanu/springjms/listener/MessageListener.java
+++ src/main/java/com/anuanu/springjms/listener/MessageListener.java
@@ -2,6 +2,8 @@ package com.anuanu.springjms.listener;
 
 import com.anuanu.springjms.config.JmsConfig;
 import com.anuanu.springjms.model.GreetingMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
@@ -11,13 +13,16 @@ import org.springframework.stereotype.Component;
 import javax.jms.Message;
 
 @Component
-public class GreetingMessageListener {
-
+public class MessageListener {
+    Logger log = LoggerFactory.getLogger(MessageListener.class);
     @JmsListener(destination = JmsConfig.QUEUE)
     public void listen(@Payload GreetingMessage greetingMessage,
                        @Headers MessageHeaders messageHeaders,
                        Message message) {
-        System.out.println("Greeting Received!!!");
-        System.out.println(greetingMessage);
+        log.info("Greeting Received!!!");
+        log.info("Greeting message {}", greetingMessage);
+        log.info("Message headers {}", messageHeaders);
+        log.info("Message {}", message);
     }
 }
+    
--- src/main/java/com/anuanu/springjms/sender/MessageSender.java
+++ src/main/java/com/anuanu/springjms/sender/MessageSender.java
@@ -1,8 +1,11 @@
 package com.anuanu.springjms.sender;
 
 import com.anuanu.springjms.config.JmsConfig;
+import com.anuanu.springjms.listener.MessageListener;
 import com.anuanu.springjms.model.GreetingMessage;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -11,14 +14,15 @@ import java.util.UUID;
 
 @RequiredArgsConstructor
 @Component
-public class GreetingSender {
+public class MessageSender {
 
+    Logger log = LoggerFactory.getLogger(MessageSender.class);
     private final JmsTemplate jmsTemplate;
     private static Integer ID = 1;
 
     @Scheduled(fixedRate = 2000)
     public void sendMessage() {
-        System.out.println("Greetings user");
+        log.info("Greetings user");
         GreetingMessage message = GreetingMessage
                 .builder()
                 .id(UUID.randomUUID())
@@ -26,6 +30,6 @@ public class GreetingSender {
                 .build();
 
         jmsTemplate.convertAndSend(JmsConfig.QUEUE, message);
-        System.out.println("Message sent!!!");
+        log.info("Message sent!!!");
     }
 }
